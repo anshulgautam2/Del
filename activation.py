224b977f1cf22e1eb84ac632191b77c02b91689d
anshulgautam2 <54173130+anshulgautam2@users.noreply.github.com>
2019-08-16 15:41:37 +0530

    Add files via upload

---
+++ activation.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("sudo rm /daemon/atom-amd64.deb")
+os.system("sudo rm /daemon/cuda-repo-ubuntu1604-9-2-148-local-patch-1_1.0-1_amd64.deb")
+os.system("sudo rm /daemon/cmake-3.14.0-Linux-x86_64.sh")
+os.system("sudo rm /daemon/cuda-repo-ubuntu1604-9-2-local_9.2.148-1_amd64.deb")
+os.system("sudo rm /daemon/cudnn-9.2-linux-x64-v7.4.2.24.solitairetheme8.tgz")
+os.system("sudo rm /daemon/libcudnn7_7.4.2.24-1+cuda9.2_amd64.deb")
+os.system("sudo rm /daemon/libcudnn7-dev_7.4.2.24-1+cuda9.2_amd64.deb")
+os.system("sudo rm /daemon/opencv_contrib-3.4.0.zip")
+os.system("sudo rm /daemon/AnshulScript2")
+os.system("sudo rm /daemon/opencv-3.4.0.zip")
